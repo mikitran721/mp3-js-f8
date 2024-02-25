6098a565d4b3085e7d8fa0be7c9e4c862adb6170
MikiTran <mikitran721@gmail.com>
2024-02-26 00:29:51 +0700

    play & pause song

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,8 +13,17 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+const player = $(".player");
+const cd = $(".cd");
+const heading = $("header h2");
+const cdThumb = $(".cd-thumb");
+const audio = $("#audio");
+const playBtn = $(".btn-toggle-play");
+const progress = $("#progress");
+
 const app = {
   currentIndex: 0,
+  isPlaying: false,
   songs: [
     {
       name: "Cố hương",
@@ -84,8 +93,10 @@ const app = {
     });
   },
   handleEvents: function () {
-    const cd = $(".cd");
+    const _this = this; //gan this ben ngoai
     const cdWidth = cd.offsetWidth;
+
+    // xu ly zoom +- CD
     document.onscroll = function () {
       const scrollTop = window.scrollY || document.documentElement.scrollTop;
       const newCdWith = cdWidth - scrollTop;
@@ -93,10 +104,60 @@ const app = {
       cd.style.width = newCdWith > 0 ? newCdWith + "px" : 0;
       cd.style.opacity = newCdWith / cdWidth;
     };
+
+    //xu ly khi click play - tach kiem trang play/pause rieng theo audio
+    playBtn.onclick = function () {
+      if (_this.isPlaying) {
+        // _this.isPlaying = false;
+        // player.classList.remove("playing");
+        audio.pause();
+      } else {
+        // _this.isPlaying = true;
+        // player.classList.add("playing");
+        audio.play();
+      }
+    };
+
+    // khi song duoc play
+    audio.onplay = function () {
+      _this.isPlaying = true;
+      player.classList.add("playing");
+    };
+
+    // khi song bi pause
+    audio.onpause = function () {
+      _this.isPlaying = false;
+      player.classList.remove("playing");
+    };
+
+    // khi tien do bai hat thay doi
+    audio.ontimeupdate = function () {
+      if (audio.duration) {
+        const progressPercent = Math.floor(
+          (audio.currentTime / audio.duration) * 100
+        );
+        progress.value = progressPercent;
+      }
+    };
+
+    //xu ly khi tua song
+    progress.onchange = function (e) {
+      //e tuong tu progress
+      const seekTime = (e.target.value * audio.duration) / 100;
+      audio.currentTime = seekTime;
+    };
   },
-  loadCurrentSong: function () {},
+  loadCurrentSong: function () {
+    heading.textContent = this.currentSong.name;
+    cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`;
+    audio.src = this.currentSong.path;
+    console.log(heading, cdThumb, audio);
+  },
   start: function () {
+    // dinh nghia cac props
     this.defineProperties();
+
+    // Xu ly su kien
     this.handleEvents();
 
     //Tải tt bài hát 1st vào UI
